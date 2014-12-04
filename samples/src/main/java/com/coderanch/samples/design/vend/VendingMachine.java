53f34511725bc7385da550422e5ed6f716da9bab
Junilu Lacar <jlacar@cisco.com>
2014-12-04 17:09:28 -0500

    Refactor - extracted constant MAX_TRAYS_PER_SHELF

--- samples/src/main/java/com/coderanch/samples/design/vend/VendingMachine.java
+++ samples/src/main/java/com/coderanch/samples/design/vend/VendingMachine.java
@@ -3,11 +3,12 @@ package com.coderanch.samples.design.vend;
 public class VendingMachine {
 
 	public static final int MAX_ITEMS_PER_TRAY = 8;
+	public static final int MAX_TRAYS_PER_SHELF = 4;
 	
-	private int[] trays = new int[4];
+	private int[] trays;
 	
 	public VendingMachine(int shelves) {
-		trays = new int[shelves * 4];
+		trays = new int[shelves * MAX_TRAYS_PER_SHELF];
 	}
 
 	public Boolean isEmpty(int tray) {

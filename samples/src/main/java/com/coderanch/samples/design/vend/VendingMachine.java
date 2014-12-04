509271cca163109960c8f654dac6ea9f96ec4e1e
Junilu Lacar <jlacar@cisco.com>
2014-12-04 15:17:03 -0500

    Make @Test topUp_should_fill_up_empty_tray pass

--- samples/src/main/java/com/coderanch/samples/design/vend/VendingMachine.java
+++ samples/src/main/java/com/coderanch/samples/design/vend/VendingMachine.java
@@ -2,11 +2,14 @@ package com.coderanch.samples.design.vend;
 
 public class VendingMachine {
 
-	public Boolean isEmpty(int slot) {
-		return true;
+	private int[] trays = new int[4];
+	
+	public Boolean isEmpty(int tray) {
+		return trays[tray] == 0;
 	}
 
-	public void topUp(int slot) {
+	public void topUp(int tray) {
+		trays[tray] = 8;
 	}
 
 }

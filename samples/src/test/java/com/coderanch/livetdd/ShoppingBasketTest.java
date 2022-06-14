768d8c4d672a6c94f6b93baee925ab4bac4c369c
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-06-14 02:15:07 -0400

    Updated maven pom to use JDK 18 and JUnit5 latest

--- samples/src/test/java/com/coderanch/livetdd/ShoppingBasketTest.java
+++ samples/src/test/java/com/coderanch/livetdd/ShoppingBasketTest.java
@@ -1,7 +1,18 @@
 package com.coderanch.livetdd;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DisplayNameGenerator.*;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
 class ShoppingBasketTest {
 
+    @Nested class zero {
+        @Test void nothing_in_cart() {
+            assertThat(1).isEqualTo(0);
+        }
+    }
 }

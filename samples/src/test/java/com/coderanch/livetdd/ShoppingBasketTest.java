87f48cd151fb249cdfe7f69ac5c10dbd010d7437
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-06-14 03:03:19 -0400

    Optimized imports

--- samples/src/test/java/com/coderanch/livetdd/ShoppingBasketTest.java
+++ samples/src/test/java/com/coderanch/livetdd/ShoppingBasketTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.DisplayNameGenerator.*;
+import static org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class ShoppingBasketTest {

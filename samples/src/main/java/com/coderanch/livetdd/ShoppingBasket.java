04fa71dafc930ca373a48222aec607521d7b5d72
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-06-17 04:09:59 -0400

    Added mobbing participants log

--- samples/src/main/java/com/coderanch/livetdd/ShoppingBasket.java
+++ samples/src/main/java/com/coderanch/livetdd/ShoppingBasket.java
@@ -1,4 +1,28 @@
 package com.coderanch.livetdd;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ShoppingBasket {
+    double price;
+    List<String> items = new ArrayList<>();
+
+    public double totalPrice() {
+        return price;
+    }
+
+    public long getQuantity(String someItem) {
+        return items.stream().filter(e -> e.equals(someItem)).count();
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public void add(String item, double unitPrice) {
+        price = unitPrice;
+        items.add(item);
+    }
 }
--- samples/src/test/java/com/coderanch/livetdd/ShoppingBasketTest.java
+++ samples/src/test/java/com/coderanch/livetdd/ShoppingBasketTest.java
@@ -10,9 +10,47 @@ import static org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class ShoppingBasketTest {
 
-    @Nested class zero {
-        @Test void nothing_in_cart() {
-            assertThat(1).isEqualTo(0);
+    ShoppingBasket basket = new ShoppingBasket();
+
+    @Nested class New_Basket {
+
+        @Test void its_total_price_is_zero() {
+            assertThat(basket.totalPrice()).isEqualTo(0.0);
         }
+
+        @Test void item_is_not_present() {
+            assertThat(basket.getQuantity("Some Item")).isEqualTo(0);
+        }
+    }
+
+    @Nested class Non_Empty {
+        @Test void with_one_item_totalPrice_is_more_than_zero() {
+            basket.add("Some Item", 5.00);
+
+            assertThat(basket.totalPrice()).isGreaterThan(0.0);
+        }
+
+        @Test void with_one_item_quantity_is_one() {
+            basket.add("Some Item", 5.00);
+
+            assertThat(basket.getQuantity("Some Item")).isEqualTo(1);
+        }
+
+        @Test void getQuantity_matches_number_of_times_same_item_was_added() {
+            int timesAdded = 5;
+            addItem("Some Item", timesAdded);
+
+            assertThat(basket.getQuantity("Some Item")).isEqualTo(timesAdded);
+        }
+    }
+
+    private void addItem(String itemName, int timesAdded) {
+        for (int i = 0; i < timesAdded; i++) {
+            basket.add(itemName, 5.00);
+        }
+    }
+
+    @Nested class Free_Items {
+
     }
 }
